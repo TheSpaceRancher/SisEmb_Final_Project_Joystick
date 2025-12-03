8b34f0614e46607bcfabc006fc1a7d62028a847e
UpaNewton <emmanuelisaac.garcia@upaep.edu.mx>
2025-12-02 23:47:08 -0600

    final

---
+++ src/LIbraryusbmodifed.h
@@ -0,0 +1,7 @@
+#include <18LF4550.h>
+/* Removed #device directive for compiler compatibility; ADC assumed 10-bit */
+
+#FUSES NOWDT                 	//No Watch Dog Timer
+
+#use delay(crystal=20000000)
+
--- src/main.h
+++
@@ -1,7 +0,0 @@
-#include <18F4550.h>
-#device ADC=10
-
-#FUSES NOWDT                 	//No Watch Dog Timer
-
-#use delay(crystal=20000000)
-
